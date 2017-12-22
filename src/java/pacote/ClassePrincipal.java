0e7a4ec148a64a7d631e619d817678870427ad7e
brenamaia <brenamaia12@gmail.com>
2017-12-22 15:25:54 -0300

    projeto web criado

---
+++ src/java/pacote/ClassePrincipal.java
@@ -0,0 +1,12 @@
+package pacote;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@SessionScoped
+@ManagedBean(name="principal")
+public class ClassePrincipal implements Serializable{
+
+}
